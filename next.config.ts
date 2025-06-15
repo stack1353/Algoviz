c65c4af22a609c4645d83f62cb87c4e2edfd78ff
Girish Goudar <girish9916853642@gmail.com>
2025-06-15 07:48:15 +0000

    again error is still there

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
